67be9b9ec719fc08826f21e49e13d5616c9819a3
Geo-fs <149318535+Geo-fs@users.noreply.github.com>
2025-04-24 14:00:37 -0500

    Create GameState.h V3

---
+++ GameState.h
@@ -0,0 +1,15 @@
+#pragma once
+#ifndef GAMESTATE_H
+#include <iostream>
+#define GAMESTATE_H
+#include <afxctl.h>
+#include <string>
+#include <thread>
+
+struct GameState {
+    int playerHealth;
+    int playerScore;
+    
+};
+
+#endif // GAMESTATE_H
